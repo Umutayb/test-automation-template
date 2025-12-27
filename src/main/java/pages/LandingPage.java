9962b2230204f3b5389cb97e77d44e78841f151f
Umut Ay Bora <umutaybora@gmail.com>
2025-12-27 10:24:21 +0100

    pickleib-2.0.8
    Updated readme!

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pickleib.web.PickleibPageObject;
-
 import java.util.List;
 
 public class LandingPage extends PickleibPageObject {
